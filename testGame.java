a625b8250f263f939daf8f7326ce48b17a345091
Dezthro <mabo7991@gmail.com>
2017-06-24 16:57:29 +0200

    Create testGame.java

---
+++ testGame.java
@@ -0,0 +1,22 @@
+import java.util.concurrent.ThreadLocalRandom;
+
+public class testGame {
+
+	public static void main(String[] args) {
+		
+		Game game = new Game();
+		
+		do {
+			
+			int tempColumn = ThreadLocalRandom.current().nextInt(7);
+			System.out.println();
+			System.out.println("new Field in Column: " + tempColumn + " from " + (!game.getFirstActive()));
+			game.add(tempColumn);
+			System.out.println();
+			System.out.println(game.toString());
+			
+		} while (!game.getGameFinished());
+		
+	}
+
+}
