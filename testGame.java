5236ff25a60edba48e5a6e707f2a9905068e7c52
Dezthro <mabo7991@gmail.com>
2017-06-25 13:59:43 +0200

    Update testGame.java

--- testGame.java
+++ testGame.java
@@ -1,21 +1,42 @@
-import java.util.concurrent.ThreadLocalRandom;
+import java.util.Scanner;
 
+/**
+ * a class which creates a test for the game
+ * 
+ * @author Marius Bonke 4839983 Gruppe 3b
+ * @author Lucas Schröder 4809832 Gruppe 3b
+ */
 public class testGame {
 
 	public static void main(String[] args) {
 		
+		Scanner scanner = new Scanner(System.in);
 		Game game = new Game();
+		SwingWindow window = new SwingWindow();
+		int tempColumn;
 		
 		do {
 			
-			int tempColumn = ThreadLocalRandom.current().nextInt(7);
-			System.out.println();
-			System.out.println("new Field in Column: " + tempColumn + " from " + game.getName(game.getActivePlayer()));
-			game.add(tempColumn);
-			System.out.println();
-			System.out.println(game.toString());
+			System.out.println("Player " + (char)(game.getActivePlayer() + 64) + "'s turn! Type in a row (from 0 to 6):");
+			tempColumn = scanner.nextInt();
+			if(tempColumn > 6) {
+				
+				System.out.println();
+				System.out.println("Try again!");
+				
+			} else {
+				
+				System.out.println();
+				System.out.println("new Field in Column: " + tempColumn + " from " + game.getName(game.getActivePlayer()));
+				game.add(tempColumn);
+				System.out.println();
+				System.out.println(game.toString());
+				
+			}
 			
 		} while (!game.getGameFinished());
+		
+		scanner.close();
 		
 	}
 
